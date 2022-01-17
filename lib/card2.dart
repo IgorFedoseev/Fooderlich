fec2eaa296799a7c9c496024822b2fafe44c6ee1
Igor Fedoseev <ifedoseev.flagman@gmail.com>
2022-01-17 22:26:57 +0300

    fooderlich

---
+++ lib/card2.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'author_card.dart';
+import 'fooderlich_theme.dart';
+
+class Card2 extends StatelessWidget {
+  const Card2({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      // 1
+      child: Container(
+        constraints: const BoxConstraints.expand(
+          width: 360,
+          height: 550,
+        ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/mag5.png'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(10.0),
+          ),
+        ),
+        // 2
+        child: Column(
+          children: [
+            const FittedBox(
+              child: AuthorCard(
+                authorName: 'Mike Katz',
+                title: 'Smoothie Connoiseur',
+                imageProvider: AssetImage('assets/author_katz.jpeg'),
+              ),
+            ),
+            // 1
+            Expanded(
+              // 2
+              child: Stack(
+                children: [
+                  // 3
+                  Positioned(
+                    bottom: 16,
+                    right: 16,
+                    child: Text(
+                      'Recipe',
+                      style: FooderlichTheme.lightTextTheme.headline1,
+                    ),
+                  ),
+                  // 4
+                  Positioned(
+                    bottom: 70,
+                    left: 16,
+                    child: RotatedBox(
+                      quarterTurns: 3,
+                      child: Text(
+                        'Smoothies',
+                        style: FooderlichTheme.lightTextTheme.headline1,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
